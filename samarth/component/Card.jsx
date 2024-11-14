96477271cb1c4299d5d8eafe8eba05d9d6670475
samarth <202351125@iiitvadodara.ac.in>
2024-11-15 01:20:46 +0530

    partially completed gallery and started login

--- samarth/component/Card.jsx
+++ samarth/component/Card.jsx
@@ -2,10 +2,10 @@ export default function Card({ li }) {
   console.log(li);
   return (
     <div
-      className="h-[158px] w-[124px] rounded-md
+      className="h-[150px] w-[124px] rounded-2xl
     "
     >
-      <img src={li} alt="" className="h-full w-20 rounded-md" />
+      <img src={li} alt="" className="h-full w-28 rounded-2xl" />
     </div>
   );
 }
---
+++ samarth/component/Card2.jsx
@@ -0,0 +1,10 @@
+export default function Card2({ li }) {
+  return (
+    <div
+      className="h-[150px] w-[124px] rounded-2xl bg-black
+    "
+    >
+      sdg
+    </div>
+  );
+}
